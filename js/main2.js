06aec85f9fd19d8f92fa8382f53b9e44838a6776
johnnybro360 <abest82@gmail.com>
2022-05-09 09:34:10 +0800

    Initial Commit

---
+++ js/main2.js
@@ -0,0 +1,46 @@
+// JavaScript Document 
+
+
+var mybutton = document.getElementById("myBtn");
+
+        window.onscroll = function () {
+          scrollFunction();
+        };
+
+        function scrollFunction() {
+          if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+          ) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+
+        function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+/*
+let mainText = document.querySelector("h2")
+
+window.addEventListener("scroll",function(){
+	let value = window.scrollY;
+	console.log("scrollY",value);
+	
+	if(value>520){
+		mainText.style.animation="slideback 1s ease-out forwards";
+	}else{
+		mainText.style.animation="slide 1s ease-out";
+	}
+});*/
